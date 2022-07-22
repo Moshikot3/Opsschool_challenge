09ff5f0b6a6114ae4e9820880fdf58b903db6994
Moshikot3 <moshikot015@gmail.com>
2022-07-22 14:59:51 +0300

    .

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #add fix to exercise3 here
+grep -v "Require all denied" /etc/apache2/sites-available/000-default.conf > tmpfile2 && mv -f tmpfile2 /etc/apache2/sites-available/000-default.conf
+service apache2 reload
