f0cf94ce51220b4bdcd3262312c61e7582a8d340
Moshikot3 <moshikot015@gmail.com>
2022-07-22 19:33:22 +0300

    Some stuff for exercise6-fix.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -4,7 +4,10 @@
 ls -l /vagrant
 ssh-keygen -b 4096 -N '' -f /home/vagrant/.ssh/id_rsa
 mkdir /vagrant/server1_key
-cp /home/vagrant/.ssh/id_rsa.pub /vagrant/server1_key/id_rsa.pub
+cp /home/vagrant/.ssh/id_rsa* /vagrant/server1_key/
 echo "StrictHostKeyChecking no" >> /home/vagrant/.ssh/config
 chown -R vagrant:vagrant /home/vagrant;
+cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+
 systemctl restart ssh
+
--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
 cat /vagrant/server1_key/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+echo "StrictHostKeyChecking no" >> /home/vagrant/.ssh/config
+cp /vagrant/server1_key/id_rsa* /home/vagrant/.ssh/
 rm -rf /vagrant/server1_key
