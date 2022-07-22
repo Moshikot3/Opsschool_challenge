da37dcdaf293b723d3b7a71b30d806c231068d63
Moshikot3 <moshikot015@gmail.com>
2022-07-22 16:30:43 +0300

    hardest exercise in the universe

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+#https://www.vagrantup.com/docs/synced-folders
+ls -l /vagrant
+ssh-keygen -b 4096 -N '' -f /home/vagrant/.ssh/id_rsa
+mkdir /vagrant/server1_key
+cp /home/vagrant/.ssh/id_rsa.pub /vagrant/server1_key/id_rsa.pub
+echo "StrictHostKeyChecking no" >> /home/vagrant/.ssh/config
+chown -R vagrant:vagrant /home/vagrant;
+systemctl restart ssh
--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+cat /vagrant/server1_key/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+rm -rf /vagrant/server1_key
