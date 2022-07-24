07334bd7a6151190a6fd0cb9f1ed467624399a44
Moshikot3 <moshikot015@gmail.com>
2022-07-24 14:11:22 +0300

    Some fixes for exercise 5 (Host *)

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -5,6 +5,7 @@ ls -l /vagrant
 ssh-keygen -b 4096 -N '' -f /home/vagrant/.ssh/id_rsa
 mkdir /vagrant/server1_key
 cp /home/vagrant/.ssh/id_rsa* /vagrant/server1_key/
+echo "Host *" >> /home/vagrant/.ssh/config
 echo "StrictHostKeyChecking no" >> /home/vagrant/.ssh/config
 chown -R vagrant:vagrant /home/vagrant;
 cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
